a263fd3d73f4a13eb9eab124c8a7736570a589b9
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-07-04 15:39:05 +0200

    Update router

--- packages/frontend/src/Router.tsx
+++ packages/frontend/src/Router.tsx
@@ -1,10 +1,16 @@
 import { ModalProvider } from "@peersyst/react-components";
 import Header from "module/common/component/navigation/Header/Header";
 import ScrollToTop from "module/common/component/navigation/ScrollToTop/ScrollToTop";
-import { DashboardRouter } from "module/dashboard/DashboardRouter";
+import { useDashboardRoutes } from "module/dashboard/DashboardRouter";
 import { Suspense } from "react";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, useRoutes } from "react-router-dom";
 import Footer from "module/common/component/navigation/Footer/Footer";
+
+const Routes = () => {
+    const dashboardRoutes = useDashboardRoutes();
+
+    return useRoutes([...dashboardRoutes]);
+};
 
 const Router = (): JSX.Element => (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
@@ -12,7 +18,7 @@ const Router = (): JSX.Element => (
             <ScrollToTop />
             <Suspense fallback={<div>Loading</div>}>
                 <Header />
-                <Routes>{DashboardRouter}</Routes>
+                <Routes />
                 <Footer />
             </Suspense>
         </ModalProvider>
