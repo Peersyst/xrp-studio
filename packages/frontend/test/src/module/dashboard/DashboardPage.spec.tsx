eda09f9e81085cfc644b13249cbc7e6c0560c6db
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-07-12 09:09:30 +0200

    Fix dashboard page

--- packages/frontend/test/src/module/dashboard/DashboardPage.spec.tsx
+++ packages/frontend/test/src/module/dashboard/DashboardPage.spec.tsx
@@ -1,10 +1,6 @@
-import { screen, waitFor } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import DashboardPage from "module/dashboard/DashboardPage";
-import { FailApiCall, render, SuccessApiCall, translate } from "test-utils";
-import * as Login from "module/auth/utils/loginCall";
-import * as Recoil from "recoil";
-import userEvent from "@testing-library/user-event";
-import { RecoilState, useRecoilState } from "recoil";
+import { render, translate } from "test-utils";
 
 describe("Login integration tests", () => {
     afterEach(() => {
@@ -14,42 +10,7 @@ describe("Login integration tests", () => {
     test("Renders the page", () => {
         render(<DashboardPage />);
         screen.getByRole("heading", { name: translate("name") });
-        expect(screen.getByRole("button", { name: "Log in" }));
+        expect(screen.getByRole("button", { name: "Sign in" }));
         expect(screen.getByTestId("ArrowIcon"));
-    });
-
-    test("Log in succeeds", async () => {
-        jest.spyOn(Login, "loginCall").mockImplementation(() => SuccessApiCall({ auth_token: "test_token" }));
-
-        render(<DashboardPage />);
-        const button = screen.getByRole("button", { name: "Log in" });
-        userEvent.click(button);
-        await waitFor(() => expect(screen.getByRole("button", { name: "Log out" })));
-    });
-
-    test("Log in fails", async () => {
-        jest.spyOn(Login, "loginCall").mockImplementation(() => FailApiCall({ code: 403, message: "Invalid credentials" }));
-
-        render(<DashboardPage />);
-        const button = screen.getByRole("button", { name: "Log in" });
-        userEvent.click(button);
-        await waitFor(() => expect(screen.getByText(JSON.stringify({ code: 403, message: "Invalid credentials" }))));
-    });
-
-    test("Log out", async () => {
-        const setAuthState = jest.fn();
-
-        jest.spyOn(Recoil, "useRecoilState").mockImplementation((atom: RecoilState<unknown>) => {
-            if (atom.key === "authState") {
-                return [{ token: "auth_token", isLogged: true }, setAuthState];
-            } else {
-                return useRecoilState(atom);
-            }
-        });
-
-        render(<DashboardPage />);
-        const button = screen.getByRole("button", { name: "Log out" });
-        userEvent.click(button);
-        expect(setAuthState).toHaveBeenCalledWith({ token: undefined, isLogged: false });
     });
 });
