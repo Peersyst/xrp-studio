4d1df29337364ed7f234e98f06e2b05a47e10ecf
quimvall <qvall@peersyst.com>
2022-07-22 11:23:15 +0200

    nft test without note

--- packages/frontend/test/src/module/nft/surface/NftCard/NftCard.spec.tsx
+++ packages/frontend/test/src/module/nft/surface/NftCard/NftCard.spec.tsx
@@ -1,11 +1,16 @@
 import { screen } from "@testing-library/react";
 import NftCard from "module/common/component/surface/NftCard/NftCard";
 import { render } from "test-utils";
+import img from "../../../../../../src/asset/image/img.png";
 
 describe("NftCard tests", () => {
-    test("Renders correctly", () => {
-        render(<NftCard title={"Contemporany Bird Fifteen"} collection={"Okay Birds Contemporany"} loading={false} />);
+    test("Renders correctly with note", () => {
+        render(<NftCard title={"Contemporany Bird Fifteen"} note={"Okay Birds Contemporany"} loading={false} image={img} to={"/nft/1"} />);
         expect(screen.getByText("Contemporany Bird Fifteen")).toBeInTheDocument();
         expect(screen.getByText("Okay Birds Contemporany")).toBeInTheDocument();
+    });
+    test("Renders correctly without note", () => {
+        render(<NftCard title={"Contemporany Bird Fifteen"} loading={false} image={img} to={"/nft/1"} />);
+        expect(screen.getByText("Contemporany Bird Fifteen")).toBeInTheDocument();
     });
 });
