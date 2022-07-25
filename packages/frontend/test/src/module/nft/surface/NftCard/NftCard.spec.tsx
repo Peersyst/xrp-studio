ee22092d0c96dd9e91acd20bf668ed0956ac4823
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-07-25 16:40:09 +0200

    Fixes

--- packages/frontend/test/src/module/nft/surface/NftCard/NftCard.spec.tsx
+++ packages/frontend/test/src/module/nft/surface/NftCard/NftCard.spec.tsx
@@ -1,16 +1,14 @@
 import { screen } from "@testing-library/react";
 import NftCard from "module/common/component/surface/NftCard/NftCard";
 import { render } from "test-utils";
-import img from "../../../../../../src/asset/image/img.png";
+import NftMock from "../../../../../__mocks__/Nft.mock";
 
-describe("NftCard tests", () => {
+describe("NftCard", () => {
+    const nftMock = new NftMock();
+
     test("Renders correctly with note", () => {
-        render(<NftCard title={"Contemporany Bird Fifteen"} note={"Okay Birds Contemporany"} loading={false} image={img} to={"/nft/1"} />);
-        expect(screen.getByText("Contemporany Bird Fifteen")).toBeInTheDocument();
-        expect(screen.getByText("Okay Birds Contemporany")).toBeInTheDocument();
-    });
-    test("Renders correctly without note", () => {
-        render(<NftCard title={"Contemporany Bird Fifteen"} loading={false} image={img} to={"/nft/1"} />);
-        expect(screen.getByText("Contemporany Bird Fifteen")).toBeInTheDocument();
+        render(<NftCard nft={nftMock} />);
+        expect(screen.getByText(nftMock.metadata.name)).toBeInTheDocument();
+        expect(screen.getByRole("img")).toHaveAttribute("alt", nftMock.metadata.name);
     });
 });
