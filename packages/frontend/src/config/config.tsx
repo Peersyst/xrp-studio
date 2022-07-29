5d45a71ba01363a9e4ef4401ce832bc138589f34
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-07-29 11:30:40 +0200

    Fix frontend config

--- packages/frontend/src/config/config.tsx
+++ packages/frontend/src/config/config.tsx
@@ -1,11 +1,24 @@
-import { createConfig } from "@peersyst/react-components";
+import { CreateConfig, createConfig } from "@peersyst/react-components";
 import lightTheme from "./theme/lightTheme";
 import darkTheme from "./theme/darkTheme";
 import prodConfig from "./config.prod.json";
 import devConfig from "./config.dev.json";
+import stagingConfig from "./config.staging.json";
+
+const envConfigs: Record<string, CreateConfig> = {
+    development: devConfig,
+    production: prodConfig,
+    staging: stagingConfig,
+};
+
+const envKey = process.env.REACT_APP_ENV_CONFIG || process.env.NODE_ENV;
+
+if (!(envKey in envConfigs)) throw new Error(`${envKey} is not a valid env config`);
+
+const envConfig = envConfigs[envKey];
 
 const config = createConfig({
-    ...(process.env.NODE_ENV === "production" ? prodConfig : devConfig),
+    ...envConfig,
     themes: {
         light: lightTheme,
         dark: darkTheme,
