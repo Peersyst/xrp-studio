4024c342917e334dadbfe44b211fcccec0399e47
quimvall <qvall@peersyst.com>
2022-08-04 16:59:07 +0200

    filled styles

--- packages/frontend/src/module/common/component/surface/Chip/Chip.styles.ts
+++ packages/frontend/src/module/common/component/surface/Chip/Chip.styles.ts
@@ -3,16 +3,13 @@ import { Chip } from "@peersyst/react-components";
 
 export const ChipRoot = styled(Chip)(
     ({ theme }) => css`
-        && {
-            background-color: ${theme.palette.black["2.5"]};
-            opacity: 0.32;
-            color: ${theme.palette.black[100]};
-
-            &.ChipLabel {
-                color: ${theme.palette.black[100]};
-                font-size: 0.875rem;
-                padding: 0.375rem 0.75rem;
-            }
+        &.Filled {
+            radius: 6px;
+            background-color: ${theme.palette.black[5]};
+            color: ${theme.palette.black[60]};
+            font-weight: 500;
+            font-size: 0.875rem;
+            padding: 0.375rem 0.75rem;
         }
     `,
 );
