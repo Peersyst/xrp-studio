c4da535bf3ab56035c9070facb121cf4d98de393
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-08-14 13:24:17 +0200

    Backdrop blur

--- packages/frontend/src/config/theme/component/Backdrop.styles.ts
+++ packages/frontend/src/config/theme/component/Backdrop.styles.ts
@@ -1,8 +1,34 @@
-import { css } from "styled-components";
+import { css, keyframes } from "styled-components";
 
-export const BackdropStyles = css({
-    ".Backdrop": {
-        backgroundColor: "rgba(33, 39, 44, 0.72)",
-        backdropFilter: "blur(16px)",
+const backdropBlurEnterKeyframes = keyframes`
+    from {
+       backdrop-filter: blur(0px)
+    }
+    to {
+        backdrop-filter: blur(16px)
+    }
+`;
+
+const backdropBlurLeaveKeyframes = keyframes`
+    from {
+        backdrop-filter: blur(16px);
+    }
+    to {
+        backdrop-filter: blur(0px)
+    }
+`;
+
+const backdropBlur = css`
+    backdrop-filter: blur(16px);
+    animation: ${backdropBlurLeaveKeyframes} 400ms;
+    &[style="transition-property: background-color; transition-duration: 400ms; transition-delay: 0ms;"] {
+        animation: ${backdropBlurEnterKeyframes} 400ms;
+    }
+`;
+
+export const BackdropStyles = css`
+    .Backdrop {
+        background-color: rgba(33, 39, 44, 0.72);
+        ${backdropBlur}
     },
-});
+`;
