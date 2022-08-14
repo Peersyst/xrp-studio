851bf52e68d087c3c2f9624903b42fcee6fb7e6e
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-08-14 16:02:41 +0200

    Fixes

--- packages/frontend/src/module/nft/NftRouter.tsx
+++ packages/frontend/src/module/nft/NftRouter.tsx
@@ -1,4 +1,3 @@
 export enum NftRoutes {
     NFTS = "/nfts/",
-    COLLECTIONS = "/collections/",
 }
--- packages/frontend/src/module/nft/component/display/NftCard/NftCard.tsx
+++ packages/frontend/src/module/nft/component/display/NftCard/NftCard.tsx
@@ -1,5 +1,5 @@
 import { WithSkeleton } from "@peersyst/react-components";
-import { NftCardProps } from "module/nft/component/surface/NftCard/NftCard.types";
+import { NftCardProps } from "module/nft/component/display/NftCard/NftCard.types";
 import BaseCard from "module/nft/component/surface/BaseCard/BaseCard";
 import { NftRoutes } from "module/nft/NftRouter";
 import { NftImage } from "./NftCard.styles";
