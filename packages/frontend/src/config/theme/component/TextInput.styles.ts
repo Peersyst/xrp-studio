532ca591db0a9f4d5217381f9794432275b7e356
Jordi Parra Crespo <jordiparra99@gmail.com>
2022-09-09 15:53:42 +0200

    feat: add size to text field

--- packages/frontend/src/config/theme/component/TextInput.styles.ts
+++ packages/frontend/src/config/theme/component/TextInput.styles.ts
@@ -2,6 +2,18 @@ import { alpha } from "@peersyst/react-utils";
 import { css } from "styled-components";
 
 export const TextInputStyles = css(({ theme }) => ({
+    ".FormControl.Md": {
+        ".TextInput": {
+            height: "2.75rem",
+            padding: "0.5rem 0.875rem",
+        },
+    },
+    ".FormControl.Lg": {
+        ".TextInput": {
+            height: "3.25rem",
+            padding: "0.75rem 1rem",
+        },
+    },
     ".Filled": {
         ".TextInput": {
             backgroundColor: theme.palette.black[80],
@@ -10,8 +22,6 @@ export const TextInputStyles = css(({ theme }) => ({
     ".TextInput": {
         color: theme.palette.black[30],
         border: "1px solid " + theme.palette.black[80],
-        height: "3.25rem",
-        padding: "0.75rem 1rem",
         transition: "color 300ms, border-color 300ms",
         input: {
             fontWeight: 500,
