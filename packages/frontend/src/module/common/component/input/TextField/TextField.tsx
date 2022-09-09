136603cc1eb550e523d48e36dddb9c8f9c823c24
Jordi Parra Crespo <jordiparra99@gmail.com>
2022-09-09 16:55:05 +0200

    feat: last feedback

--- packages/frontend/src/module/common/component/input/TextField/TextField.tsx
+++ packages/frontend/src/module/common/component/input/TextField/TextField.tsx
@@ -2,10 +2,10 @@ import { cx, capitalize } from "@peersyst/react-utils";
 import { TextFieldProps } from "./TextField.types";
 import { TextField as BaseTextField } from "@peersyst/react-components";
 
-const TextField = ({ variant = "outlined", size = "lg", showValid = true, className, ...rest }: TextFieldProps): JSX.Element => {
+const TextField = ({ variant = "outlined", size = "lg", className, ...rest }: TextFieldProps): JSX.Element => {
     const appearanceClassName = capitalize(variant);
     const sizeClassName = capitalize(size);
-    return <BaseTextField {...rest} className={cx(className, appearanceClassName, sizeClassName)} showValid={showValid} />;
+    return <BaseTextField {...rest} className={cx(className, appearanceClassName, sizeClassName)} />;
 };
 
 export default TextField;
