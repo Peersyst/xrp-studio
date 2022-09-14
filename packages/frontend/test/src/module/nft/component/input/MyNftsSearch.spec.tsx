011dcf4253561573aa4832a7a30bfbf9b61a6f51
Jordi Parra Crespo <jordiparra99@gmail.com>
2022-09-14 09:29:15 +0200

    test: mynftssearch + mynftspageheader

---
+++ packages/frontend/test/src/module/nft/component/input/MyNftsSearch.spec.tsx
@@ -0,0 +1,29 @@
+import MyNftsSearch from "module/nft/component/input/MyNftsSearch/MyNftsSearch";
+import { render, translate, waitFor } from "test-utils";
+import * as UseWallet from "module/wallet/component/hooks/useWallet";
+import userEvent from "@testing-library/user-event";
+describe("MyNftsSearch ", () => {
+    test("Renders correctly", () => {
+        const screen = render(<MyNftsSearch />);
+        expect(screen.getByTestId("SearchIcon")).toBeInTheDocument();
+        const input = screen.getByRole("textbox");
+        expect(input).toBeInTheDocument();
+        expect(input).toHaveAttribute("placeholder", translate("search"));
+    });
+    test("Calls the update query fn correctly", async () => {
+        jest.spyOn(UseWallet, "default").mockReturnValue({
+            address: "0x123",
+            active: true,
+            isLogged: true,
+        });
+        const screen = render(<MyNftsSearch />);
+        expect(screen.getByTestId("SearchIcon")).toBeInTheDocument();
+        const input = screen.getByRole("textbox");
+        userEvent.type(input, "test");
+        expect(input).toHaveValue("test");
+        await waitFor(() => {
+            expect(screen.getByTestId("LoaderIcon")).toBeInTheDocument();
+        });
+        //TODO: add test for the query
+    });
+});
