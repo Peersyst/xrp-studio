4df116ca4afe1d667567e5c282347eb016c37283
JordiParraCrespo <jordiparra99@gmail.com>
2022-09-19 18:07:42 +0200

    MyNftsPageContent

--- packages/frontend/src/module/nft/pages/MyNftsPage.tsx
+++ packages/frontend/src/module/nft/pages/MyNftsPage.tsx
@@ -1,25 +1,13 @@
 import BasePage from "module/common/component/layout/BasePage/BasePage";
-import PageContent from "module/common/component/layout/PageContent/PageContent";
-import BaseNftsGrid from "../component/layout/BaseNftGrid/BaseNftGrid";
+import MyNftsPageContent from "../component/layout/MyNftsPageContent/MyNftsPageContent";
 import MyNftsPageHeader from "../component/layout/MyNftsPageHeader/MyNftsPageHeader";
-import { useGetMyNfts } from "../query/useGetMyNfts";
 
 const MyNftsPage = (): JSX.Element => {
-    const { data, hasNextPage, fetchNextPage, isFetching } = useGetMyNfts();
     return (
         <BasePage>
             {{
                 header: <MyNftsPageHeader />,
-                content: (
-                    <PageContent>
-                        <BaseNftsGrid
-                            data={data}
-                            callback={() => fetchNextPage({ cancelRefetch: false })}
-                            end={!hasNextPage}
-                            loading={isFetching}
-                        />
-                    </PageContent>
-                ),
+                content: <MyNftsPageContent />,
             }}
         </BasePage>
     );
