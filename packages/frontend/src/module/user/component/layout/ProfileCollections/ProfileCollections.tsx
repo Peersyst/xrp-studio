592f88424619c560b450b049cbfaa6c481a82127
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-09-28 14:01:57 +0200

    Nothing to show

--- packages/frontend/src/module/user/component/layout/ProfileCollections/ProfileCollections.tsx
+++ packages/frontend/src/module/user/component/layout/ProfileCollections/ProfileCollections.tsx
@@ -1,10 +1,27 @@
 import CollectionCardCarousel from "module/collection/component/display/CollectionCardCarousel/CollectionCardCarousel";
 import { useGetUserCollections } from "module/user/query/useGetUserCollections";
+import NothingToShow from "module/common/component/feedback/NothingToShow/NothingToShow";
+import { Button, Col, Row, Typography } from "@peersyst/react-components";
 
 const ProfileCollections = (): JSX.Element => {
     const { data: { pages } = { pages: [] }, isLoading } = useGetUserCollections();
     const collections = pages[0]?.items || [];
-    return collections.length === 0 && !isLoading ? <></> : <CollectionCardCarousel collections={collections} isLoading={isLoading} />;
+    return (
+        <Row css={{ height: "12rem", width: "100%" }}>
+            {collections.length === 0 && !isLoading ? (
+                <NothingToShow>
+                    <Col flex={1} alignItems="center" justifyContent="center" gap="2rem">
+                        <Typography variant="h6" fontWeight="bold" light>
+                            You have no collections
+                        </Typography>
+                        <Button>Create collection</Button>
+                    </Col>
+                </NothingToShow>
+            ) : (
+                <CollectionCardCarousel collections={collections} isLoading={!isLoading} />
+            )}
+        </Row>
+    );
 };
 
 export default ProfileCollections;
--- packages/frontend/src/module/user/component/layout/ProfileNftsGrid/ProfileNftsGrid.tsx
+++ packages/frontend/src/module/user/component/layout/ProfileNftsGrid/ProfileNftsGrid.tsx
@@ -1,5 +1,7 @@
 import NftGrid from "module/nft/component/layout/NftGrid/NftGrid";
 import { useGetProfileNfts } from "module/user/query/useGetProfileNfts";
+import NothingToShow from "module/common/component/feedback/NothingToShow/NothingToShow";
+import { Button, Col, Typography } from "@peersyst/react-components";
 
 const ProfileNftsGrid = (): JSX.Element => {
     const { data, hasNextPage, fetchNextPage, isFetching } = useGetProfileNfts();
@@ -11,6 +13,16 @@ const ProfileNftsGrid = (): JSX.Element => {
             end={!hasNextPage}
             loading={isFetching}
             filters={<>Here profile filters</>}
+            nothingToShow={
+                <NothingToShow>
+                    <Col flex={1} alignItems="center" justifyContent="center" gap="2rem" css={{ height: "12rem" }}>
+                        <Typography variant="h6" fontWeight="bold" light>
+                            You have no Nfts
+                        </Typography>
+                        <Button>Create and NFT</Button>
+                    </Col>
+                </NothingToShow>
+            }
         />
     );
 };
