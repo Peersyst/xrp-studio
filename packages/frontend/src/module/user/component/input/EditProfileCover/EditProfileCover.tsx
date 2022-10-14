92af6ee7d44db55fe4c56be607a5bfa37a39bb4c
JordiParraCrespo <jordiparra99@gmail.com>
2022-10-14 18:57:32 +0200

    wip

--- packages/frontend/src/module/user/component/input/EditProfileCover/EditProfileCover.tsx
+++ packages/frontend/src/module/user/component/input/EditProfileCover/EditProfileCover.tsx
@@ -1,13 +1,17 @@
+import { useFormNotification } from "@peersyst/react-components";
 import { cx } from "@peersyst/react-utils";
 import useGetWalletUser from "module/user/query/useGetWalletUser";
+import { useState } from "react";
 import { EditProfileCoverRoot } from "./EditProfileCover.styles";
 import { EditProfileCoverProps } from "./EditProfileCover.types";
 
 const EditProfileCover = ({ className, style }: EditProfileCoverProps): JSX.Element => {
     const { data: user = { header: "" }, isFetching } = useGetWalletUser();
-    const handleOnChange = (file: string) => {
-        console.warn(file);
+    const [url, setUrl] = useState<string>();
+    const handleOnChange = (url: string) => {
+        setUrl(url);
     };
+    useFormNotification("header", url);
 
     return (
         <EditProfileCoverRoot
