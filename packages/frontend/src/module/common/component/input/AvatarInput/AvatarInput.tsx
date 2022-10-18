0385173a4046ee374132a41148c0473c09acad04
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-10-18 08:15:21 +0200

    Fix dodder Jenkins type in test

--- packages/frontend/src/module/common/component/input/AvatarInput/AvatarInput.tsx
+++ packages/frontend/src/module/common/component/input/AvatarInput/AvatarInput.tsx
@@ -5,7 +5,8 @@ import ImageInput from "module/common/component/input/ImageInput/ImageInput";
 const AvatarInput = ({ size, alt, ...rest }: AvatarInputProps): JSX.Element => {
     return (
         <ImageInput alt={alt} {...rest}>
-            {(url) => <Avatar size={size} img={url} alt={alt} />}
+            {/* string | undefined has to be defined, otherwise dodder Jenkins complains */}
+            {(url: string | undefined) => <Avatar size={size} img={url} alt={alt} />}
         </ImageInput>
     );
 };
