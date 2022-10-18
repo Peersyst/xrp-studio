47279a5a862368c7b6dde159042aa76f0c883319
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-10-18 08:34:29 +0200

    Fix tests

--- packages/frontend/src/module/common/component/input/AvatarInput/AvatarInput.tsx
+++ packages/frontend/src/module/common/component/input/AvatarInput/AvatarInput.tsx
@@ -5,8 +5,7 @@ import ImageInput from "module/common/component/input/ImageInput/ImageInput";
 const AvatarInput = ({ size, alt, ...rest }: AvatarInputProps): JSX.Element => {
     return (
         <ImageInput alt={alt} {...rest}>
-            {/* string | undefined has to be defined, otherwise dodder Jenkins complains */}
-            {(url: string | undefined) => <Avatar size={size} img={url} alt={alt} />}
+            {(url) => <Avatar size={size} img={url} alt={alt} />}
         </ImageInput>
     );
 };
