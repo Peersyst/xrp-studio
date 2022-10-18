413d2f127221c5eb971728f0b4b05a966fcc6e5c
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-10-18 11:41:02 +0200

    TextArea refactor

--- packages/frontend/src/module/common/component/input/TextArea/TextArea.tsx
+++ packages/frontend/src/module/common/component/input/TextArea/TextArea.tsx
@@ -1,7 +1,6 @@
 import { cx, capitalize } from "@peersyst/react-utils";
 import { TextAreaProps } from "./TextArea.types";
 import { TextArea as BaseTextArea } from "@peersyst/react-components";
-import { useState } from "react";
 import { useControlled } from "@peersyst/react-hooks";
 
 const TextArea = ({
@@ -15,20 +14,21 @@ const TextArea = ({
     ...rest
 }: TextAreaProps): JSX.Element => {
     const appearanceClassName = capitalize(variant);
-    const [length, setLength] = useState(defaultValue.length);
     const [value, setValue] = useControlled<string>(defaultValue, valueProp, onChange);
-    const handleChange = (e: string) => {
-        setValue(e);
-        setLength(e.length);
+    const length = value.length;
+
+    const handleChange = (newVal: string) => {
+        const newLength = newVal.length;
+        if (maxLength !== undefined && maxLength >= newLength) setValue(newVal);
     };
 
     return (
         <BaseTextArea
-            hint={displayLength ? `${maxLength ? length + "/" + maxLength : length}` : ""}
-            {...rest}
+            hint={displayLength ? `${maxLength !== undefined ? length + "/" + maxLength : length}` : ""}
             onChange={handleChange}
             value={value}
             className={cx(className, appearanceClassName)}
+            {...rest}
         />
     );
 };
--- packages/frontend/src/module/common/component/input/TextField/TextField.types.ts
+++ packages/frontend/src/module/common/component/input/TextField/TextField.types.ts
@@ -2,13 +2,15 @@ import { TextFieldProps as BaseTextFieldProps } from "@peersyst/react-components
 
 export type TextFieldVariant = "outlined" | "filled";
 
+export type TextFieldSize = "lg" | "md";
+
 export interface TextFieldProps extends BaseTextFieldProps {
     /**
      * Text field appearance
      */
     variant?: TextFieldVariant;
     /**
-     * Size of the select
+     * Size of the TextField
      */
-    size?: "lg" | "md";
+    size?: TextFieldSize;
 }
