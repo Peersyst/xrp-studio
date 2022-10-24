7e92c3415e07e842f4c9e6886d24cad482c733e8
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-10-24 12:10:56 +0200

    CollectionRouter

--- packages/frontend/src/module/collection/CollectionRouter.tsx
+++ packages/frontend/src/module/collection/CollectionRouter.tsx
@@ -1,4 +1,22 @@
+import { RouteObject } from "react-router-dom";
+import CollectionsPage from "module/collection/pages/CollectionsPage";
+
 export enum CollectionRoutes {
     COLLECTIONS = "/collections/",
+    MY_COLLECTIONS = "/collections/my-collections",
     CREATE_COLLECTION = "/collections/creation",
 }
+
+export const useCollectionRoutes = (): RouteObject[] => {
+    return [
+        {
+            path: CollectionRoutes.MY_COLLECTIONS,
+            children: [
+                {
+                    path: CollectionRoutes.MY_COLLECTIONS,
+                    element: <CollectionsPage />,
+                },
+            ],
+        },
+    ];
+};
