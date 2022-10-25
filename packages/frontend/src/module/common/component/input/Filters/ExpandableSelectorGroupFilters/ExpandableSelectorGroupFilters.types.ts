0ed22f4a8aa3851b0230d03ecd706a9588a6ff91
JordiParraCrespo <jordiparra99@gmail.com>
2022-10-25 14:18:01 +0200

    wip

--- packages/frontend/src/module/common/component/input/Filters/ExpandableSelectorGroupFilters/ExpandableSelectorGroupFilters.types.ts
+++ packages/frontend/src/module/common/component/input/Filters/ExpandableSelectorGroupFilters/ExpandableSelectorGroupFilters.types.ts
@@ -1,8 +1,11 @@
 import { ExpandableFiltersProps } from "../ExpandableFilters/ExpandableFilters.types";
 import { SelectorGroupFilterProps } from "../SelectorGroupFilter/SelectorGroupFilter.types";
 
-export type ExpandableSelectorGroupFiltersProps<T, Name extends string> = Pick<ExpandableFiltersProps, "title"> &
-    Omit<SelectorGroupFilterProps<T>, "children" | "options" | "name" | "onChange"> &
-    Required<Pick<SelectorGroupFilterProps<T>, "options">> & {
+export type ExpandableSelectorGroupFiltersProps<T, Name extends string, Multiple extends boolean = false> = Pick<
+    ExpandableFiltersProps,
+    "title"
+> &
+    Omit<SelectorGroupFilterProps<T, Multiple>, "children" | "options" | "name" | "onChange"> &
+    Required<Pick<SelectorGroupFilterProps<T, Multiple>, "options">> & {
         name: Name;
     };
--- packages/frontend/src/module/common/component/input/Filters/SelectorGroupFilter/SelectorGroupFilter.types.ts
+++ packages/frontend/src/module/common/component/input/Filters/SelectorGroupFilter/SelectorGroupFilter.types.ts
@@ -1,5 +1,5 @@
 import { SelectorGroupProps } from "@peersyst/react-components";
 
-export interface SelectorGroupFilterProps<T> extends Omit<SelectorGroupProps<T>, "name"> {
+export interface SelectorGroupFilterProps<T, Multiple extends boolean> extends Omit<SelectorGroupProps<T, Multiple>, "name"> {
     name: string;
 }
