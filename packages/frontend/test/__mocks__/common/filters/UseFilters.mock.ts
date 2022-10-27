053104be25512921c21623803a2c223169e6238b
JordiParraCrespo <jordiparra99@gmail.com>
2022-10-27 10:12:28 +0200

    fix linting

--- packages/frontend/test/__mocks__/common/filters/UseFilters.mock.ts
+++ packages/frontend/test/__mocks__/common/filters/UseFilters.mock.ts
@@ -3,11 +3,12 @@ import * as useFilters from "module/common/component/input/Filters/hooks/useFilt
 import BaseMock from "../../base.mock";
 
 export class UseFilterMock<T extends FiltersBaseContextValue> extends BaseMock implements FiltersContext<T> {
-    filters: any;
+    filters: T;
     setFilters: (newFilters: Partial<FiltersBaseContextValue>) => void;
-    constructor({ filters = {}, setFilters = jest.fn() }: Partial<FiltersContext<{}>> = {}) {
+
+    constructor({ filters = {}, setFilters = jest.fn() }: Partial<FiltersContext<FiltersBaseContextValue>> = {}) {
         super();
-        this.filters = filters;
+        this.filters = filters as T;
         this.setFilters = setFilters;
         jest.spyOn(useFilters, "default").mockReturnValue(this);
     }
