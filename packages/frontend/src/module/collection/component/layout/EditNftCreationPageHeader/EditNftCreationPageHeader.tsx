fbbd09319fc6c8577976e03bea7f596e26846ac6
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-11-02 09:38:11 +0100

    BackButton with path prop

--- packages/frontend/src/module/collection/component/layout/EditNftCreationPageHeader/EditNftCreationPageHeader.tsx
+++ packages/frontend/src/module/collection/component/layout/EditNftCreationPageHeader/EditNftCreationPageHeader.tsx
@@ -2,6 +2,7 @@ import useTranslate from "module/common/hook/useTranslate";
 import MainPageHeader from "module/common/component/layout/MainPageHeader/MainPageHeader";
 import { Row } from "@peersyst/react-components";
 import Button from "module/common/component/input/Button/Button";
+import { CollectionRoutes } from "module/collection/CollectionRouter";
 
 const EditNftCreationPageHeader = () => {
     const translate = useTranslate();
@@ -11,6 +12,7 @@ const EditNftCreationPageHeader = () => {
             title={translate("editNft")}
             back
             backIconSize="md"
+            backPath={CollectionRoutes.CREATE_COLLECTION}
             complement={
                 <Row>
                     <Button type="submit">{translate("saveChanges")}</Button>
--- packages/frontend/src/module/common/hook/useGoBack.ts
+++ packages/frontend/src/module/common/hook/useGoBack.ts
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { DashboardRoutes } from "module/dashboard/DashboardRouter";
 
-export default function (): () => void {
+export default function (path?: string): () => void {
     const navigate = useNavigate();
 
     return () => {
+        if (path) navigate(path);
         //Verify not leaving the site
-        if (!window.history?.state || window.history?.state?.idx === 0) {
+        else if (!window.history?.state || window.history?.state?.idx === 0) {
             navigate(DashboardRoutes.MAIN);
         } else {
             navigate(-1);
