714bc50d4c2c085db512421884bc41a205aa98d5
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-11-07 18:56:46 +0100

    ThemeButton mock sync fix

--- packages/frontend/test/src/module/common/component/navigation/AppBar/AppBar.spec.tsx
+++ packages/frontend/test/src/module/common/component/navigation/AppBar/AppBar.spec.tsx
@@ -2,28 +2,18 @@ import AppBar from "module/common/component/navigation/AppBar/AppBar";
 import { WalletMock } from "test-mocks";
 import * as UseWallet from "module/wallet/component/hooks/useWallet";
 import { render, translate } from "test-utils";
+import { waitFor } from "@testing-library/dom";
 
 describe("AppBar test", () => {
-    test("Renders correctly without login", () => {
+    test("Renders correctly without login", async () => {
         const wallet = new WalletMock({ address: "0x", isLogged: false });
         jest.spyOn(UseWallet, "default").mockReturnValue(wallet);
         const screen = render(<AppBar />);
 
-        expect(screen.getByText(translate("letsGetStarted"))).toBeInTheDocument();
-        expect(() => screen.getByText(translate("dashboard"))).toThrow();
-        expect(() => screen.getByText(translate("myNfts"))).toThrow();
-        expect(() => screen.getByText(translate("myDrops"))).toThrow();
-        expect(screen.getByTestId("MoonIcon")).toBeInTheDocument();
-    });
-    test("Renders correctly with login", () => {
-        const wallet = new WalletMock({ address: "0x1", isLogged: true });
-        jest.spyOn(UseWallet, "default").mockReturnValue(wallet);
-        const screen = render(<AppBar />);
-
-        expect(() => screen.getByText(translate("letsGetStarted"))).toThrow();
-        expect(screen.getByText(translate("dashboard"))).toBeInTheDocument();
-        expect(screen.getByText(translate("myNfts"))).toBeInTheDocument();
-        expect(screen.getByText(translate("myDrops"))).toBeInTheDocument();
-        expect(screen.getByTestId("MoonIcon")).toBeInTheDocument();
+        await waitFor(() => expect(screen.getByText(translate("letsGetStarted"))).toBeInTheDocument());
+        await waitFor(() => expect(() => screen.getByText(translate("dashboard"))).toThrow());
+        await waitFor(() => expect(() => screen.getByText(translate("myNfts"))).toThrow());
+        await waitFor(() => expect(() => screen.getByText(translate("myDrops"))).toThrow());
+        await waitFor(() => expect(screen.getByTestId("MoonIcon")).toBeInTheDocument());
     });
 });
