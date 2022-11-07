62d91e506a17da1db39aa3f67900b7ef29b62936
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-11-07 18:31:13 +0100

    ThemeButton mock sync fix

--- packages/frontend/test/src/module/common/component/input/ThemeButton/ThemeButton.tsx
+++ packages/frontend/test/src/module/common/component/input/ThemeButton/ThemeButton.tsx
@@ -2,15 +2,16 @@ import * as ReactComponents from "@peersyst/react-components";
 import { render } from "test-utils";
 import userEvent from "@testing-library/user-event";
 import AppBar from "module/common/component/navigation/AppBar/AppBar";
+import { waitFor } from "@testing-library/dom";
 
 describe("ThemeButton test", () => {
-    test("Call setTheme correctly", () => {
+    test("Call setTheme correctly", async () => {
         const mockedValue = jest.fn();
         jest.spyOn(ReactComponents, "useSetTheme").mockReturnValue(mockedValue);
 
         const screen = render(<AppBar />);
         const button = screen.getByTestId("MoonIcon");
         userEvent.click(button);
-        expect(mockedValue).toBeCalled();
+        await waitFor(() => expect(mockedValue).toBeCalled());
     });
 });
