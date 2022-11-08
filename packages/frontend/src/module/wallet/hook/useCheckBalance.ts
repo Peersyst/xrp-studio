2d5266bbbd06c92f20d1c7e93087db8705b8443d
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-11-08 16:13:02 +0100

    useCheckBalance comprobation in NftCreationPage

--- packages/frontend/src/module/wallet/hook/useCheckBalance.ts
+++ packages/frontend/src/module/wallet/hook/useCheckBalance.ts
@@ -1,23 +1,15 @@
-import { useToast } from "@peersyst/react-components";
 import XrplService from "module/blockchain/service/XrplService/XrplService";
 import useWallet from "module/wallet/component/hooks/useWallet";
-import useTranslate from "module/common/hook/useTranslate";
 import { useQuery } from "react-query";
 
-export const useCheckBalance = () => {
-    const { showToast } = useToast();
+export default function () {
     const xrplService = new XrplService();
     const { address } = useWallet();
-    const translate = useTranslate("error");
     return useQuery(["has-balance", address], async () => {
         try {
-            const hasBalance = await xrplService.hasEnoughBalance(address ?? "");
-            if (!hasBalance) {
-                showToast(translate("notEnoughtBalance"), { type: "error" });
-            }
-            return hasBalance;
+            return await xrplService.hasEnoughBalance(address ?? "");
         } catch (e) {
             return false;
         }
     });
-};
+}
