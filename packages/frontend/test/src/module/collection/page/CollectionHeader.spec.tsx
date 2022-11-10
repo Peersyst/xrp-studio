e51e887377ff6593c0bcae0732b5da055156efd4
pablo <silvap.javier@gmail.com>
2022-11-10 18:05:18 +0100

    Fix test

--- packages/frontend/test/src/module/collection/page/CollectionHeader.spec.tsx
+++ packages/frontend/test/src/module/collection/page/CollectionHeader.spec.tsx
@@ -1,18 +1,20 @@
-import { CollectionService, NftService } from "module/api/service";
+import { CollectionService } from "module/api/service";
 import CollectionHeader from "module/collection/component/layout/CollectionHeader/CollectionHeader";
-import { CollectionDtoMock, NftDtoMock, NftsDtoMock, PaginatedDataMock } from "test-mocks";
+import { CollectionDtoMock } from "test-mocks";
 import { render, translate, waitFor } from "test-utils";
+import * as Router from "react-router-dom";
 
 describe("Test for the Collection Header", () => {
-    describe("Collection Heade render", () => {
+    describe("Collection Header render", () => {
         test("Renders Information of Collection", async () => {
+            jest.spyOn(Router, "useParams").mockReturnValue({ id: "1" });
             const collectionMock = new CollectionDtoMock();
             jest.spyOn(CollectionService, "collectionControllerGetCollection").mockResolvedValue(collectionMock);
-            const data = new PaginatedDataMock<NftDtoMock[]>({ items: new NftsDtoMock({ length: 10 }).nfts });
-            jest.spyOn(NftService, "nftControllerGetNfts").mockResolvedValue(data);
+
             const screen = render(<CollectionHeader />);
 
-            await waitFor(() => expect(screen.getByText(collectionMock.name!)).toBeInTheDocument());
+            //Se comprueba con toHaveLength 2 , por que renderiza el header y el sticker.
+            await waitFor(() => expect(screen.getAllByText(collectionMock.name!)).toHaveLength(2));
 
             const images = screen.getAllByRole("img");
             expect(images.some((image) => image.getAttribute("alt") === "collection-image")).toBe(true);
