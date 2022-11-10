1e8f1b8bfbc8425a81b4837a5a708a0c4f538a41
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-11-10 12:07:22 +0100

    PublishModal from ModalRoot

--- packages/frontend/src/module/common/component/feedback/PublishModal/PublishModal.tsx
+++ packages/frontend/src/module/common/component/feedback/PublishModal/PublishModal.tsx
@@ -1,22 +1,47 @@
 import { ReactElement } from "react";
-import Modal from "module/common/component/feedback/Modal/Modal";
-import { Col, createModal, Row } from "@peersyst/react-components";
-import { ModalProps } from "module/common/component/feedback/Modal/Modal.types";
+import { Col, Row, Typography } from "@peersyst/react-components";
+import { CloseModalButton, ModalRoot } from "module/common/component/feedback/Modal/Modal.styles";
+import { ModalProps as BaseModalProps } from "@peersyst/react-components";
+import { useControlled } from "@peersyst/react-hooks";
+import CloseIcon from "../../../icons/CloseIcon";
 
-export interface PublishModalProps {
+export interface PublishModalProps extends Omit<BaseModalProps, "children"> {
+    title?: string;
     children: { cover: ReactElement; information: ReactElement };
-    modalProps: ModalProps;
 }
 
-export const PublishModal = createModal((modalProps, { children: { cover, information } }): JSX.Element => {
+const PublishModal = ({
+    open: openProp,
+    title,
+    elevation = 3,
+    onClose,
+    closable = true,
+    children: { cover, information },
+    ...rest
+}: PublishModalProps): JSX.Element => {
+    const [open, setOpen] = useControlled(true, openProp, openProp ? onClose : undefined);
     return (
-        <Modal {...modalProps}>
-            <Col>
-                <Row>{cover}</Row>
-                <Row>{information}</Row>
+        <ModalRoot open={open} onClose={() => setOpen(false)} elevation={elevation} closable={closable} {...rest}>
+            <Col gap="3rem" className="modal-main-col">
+                {title && (
+                    <Col>
+                        <Typography variant="h3" fontWeight={700} className="modal-title">
+                            {title}
+                        </Typography>
+                    </Col>
+                )}
+                {closable && (
+                    <CloseModalButton onClick={() => setOpen(false)}>
+                        <CloseIcon />
+                    </CloseModalButton>
+                )}
+                <Row gap={4} flex={1} justifyContent="center">
+                    <Col>{cover}</Col>
+                    <Col>{information}</Col>
+                </Row>
             </Col>
-        </Modal>
+        </ModalRoot>
     );
-});
+};
 
 export default PublishModal;
