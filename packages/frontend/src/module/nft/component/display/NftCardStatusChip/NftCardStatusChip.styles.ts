4360fc9395b07a419d740c73ca27585463e42188
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-11-11 08:00:31 +0100

    Fixes

--- packages/frontend/src/module/nft/component/display/NftCardStatusChip/NftCardStatusChip.styles.ts
+++ packages/frontend/src/module/nft/component/display/NftCardStatusChip/NftCardStatusChip.styles.ts
@@ -1,48 +1,35 @@
 import { Chip } from "@peersyst/react-components";
-import { NftStatus } from "./NftCardStatusChip.types";
 import { Popover } from "@peersyst/react-components";
 import Card from "module/common/component/surface/Card/Card";
 import styled, { css } from "styled-components";
 
-const draftStyles = css(({ theme }) => ({
-    backgroundColor: theme.palette.status.info,
-}));
-
-const pendingStyles = css(({ theme }) => ({
-    backgroundColor: theme.palette.status.warning,
-}));
-
-const failedStyles = css(({ theme }) => ({
-    backgroundColor: `${theme.palette.status.error} !important`,
-    color: `${theme.palette.black[100]} !important`,
-}));
-
-const confirmedStyles = css(() => ({
-    display: "none",
-}));
-
-const variantStyles: Record<NftStatus, ReturnType<typeof css>> = {
-    draft: draftStyles,
-    pending: pendingStyles,
-    failed: failedStyles,
-    confirmed: confirmedStyles,
-};
-
-export const NftCardChip = styled(Chip)<{ status: NftStatus }>(
-    ({ status }) => css`
-        position: absolute;
-        top: 0.5rem;
-        left: 0.5rem;
+export const NftCardChip = styled(Chip)(
+    ({ theme }) => css`
         text-transform: capitalize;
-        ${variantStyles[status!]};
+
+        &&& {
+            color: ${theme.palette.text};
+
+            &.draft {
+                background-color: ${theme.palette.black[75]};
+            }
+
+            &.pending {
+                background-color: ${theme.palette.status.warning};
+            }
+
+            &.failed {
+                background-color: ${theme.palette.status.error};
+            }
+        }
     `,
 );
 
 export const NftStatusChipRoot = styled(Popover)(
     () => css`
         position: absolute;
+        top: 0.5rem;
         left: 0.5rem;
-        top: 0.2rem;
     `,
 );
 
