3e417559ec503f1629f4cba4fe643030b9760866
JordiParraCrespo <jordiparra99@gmail.com>
2022-11-11 10:48:19 +0100

    fix

--- packages/frontend/src/module/wallet/component/navigation/LoggedInRoute/LoggedInRoute.tsx
+++ packages/frontend/src/module/wallet/component/navigation/LoggedInRoute/LoggedInRoute.tsx
@@ -1,7 +1,7 @@
-import useWallet from "module/wallet/component/hooks/useWallet";
 import { PropsWithChildren } from "react";
 import ProtectedRoute from "module/common/component/navigation/ProtectedRoute/ProtectedRoute";
 import { DashboardRoutes } from "module/dashboard/DashboardRouter";
+import useWallet from "module/wallet/hook/useWallet";
 
 const LoggedInRoute = ({ children }: PropsWithChildren<{}>): JSX.Element => {
     const { isLogged } = useWallet();
