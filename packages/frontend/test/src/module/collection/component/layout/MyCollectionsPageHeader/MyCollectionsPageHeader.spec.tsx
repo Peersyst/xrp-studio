fafcc7592a5f6ffbd87ec57b2aa4821446a0917f
JordiParraCrespo <jordiparra99@gmail.com>
2022-11-14 11:12:35 +0100

    My collections page header test

--- packages/frontend/test/src/module/collection/component/layout/MyCollectionsPageHeader/MyCollectionsPageHeader.spec.tsx
+++ packages/frontend/test/src/module/collection/component/layout/MyCollectionsPageHeader/MyCollectionsPageHeader.spec.tsx
@@ -1,14 +1,17 @@
-import { render, translate } from "test-utils";
+import { act, render, translate, waitFor } from "test-utils";
 import MyCollectionsPageHeader from "module/collection/component/layout/MyCollectionsPageHeader/MyCollectionsPageHeader";
 import * as ReactRouterDom from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 import { CollectionRoutes } from "module/collection/CollectionRouter";
+import { UseFilterMock } from "test-mocks";
 
 describe("Test for MyCollectionsPageHeader", () => {
     test("Renders correctly", () => {
         const screen = render(<MyCollectionsPageHeader />);
         expect(screen.getByRole("heading", { name: translate("myCollections") })).toBeInTheDocument();
         expect(screen.getByRole("button", { name: translate("createCollection") })).toBeInTheDocument();
+        const input = screen.getByRole("textbox");
+        expect(input).toBeInTheDocument();
     });
     test("Navigates to the create collection page", () => {
         const mockedNavigate = jest.fn();
@@ -17,5 +20,17 @@ describe("Test for MyCollectionsPageHeader", () => {
         const button = screen.getByRole("button", { name: translate("createCollection") });
         userEvent.click(button);
         expect(mockedNavigate).toHaveBeenCalledWith(CollectionRoutes.CREATE_COLLECTION);
+    });
+    test("Update the query filter correctly", async () => {
+        const { setFilter } = new UseFilterMock();
+        const screen = render(<MyCollectionsPageHeader />);
+        const input = screen.getByRole("textbox");
+        expect(input).toBeInTheDocument();
+        expect(input).toHaveAttribute("placeholder", translate("search"));
+        expect(screen.getByTestId("SearchIcon")).toBeInTheDocument();
+        userEvent.type(input, "test");
+        await act(async () => {
+            await waitFor(() => expect(setFilter).toHaveBeenCalledWith("test"));
+        });
     });
 });
