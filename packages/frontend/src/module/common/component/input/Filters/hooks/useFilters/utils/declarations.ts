bb9784f4154a5e0eb0fe1c4389d2763464fee6d5
JordiParraCrespo <jordiparra99@gmail.com>
2022-11-15 11:00:28 +0100

    fix declaration

--- packages/frontend/src/module/common/component/input/Filters/hooks/useFilters/utils/declarations.ts
+++ packages/frontend/src/module/common/component/input/Filters/hooks/useFilters/utils/declarations.ts
@@ -1,12 +1,11 @@
 import { BaseFiltersNames, Order } from "module/common/component/input/Filters/Filters.types";
-import { Filters } from "../useFilters.types";
 
 export type BaseFilters = {
     [BaseFiltersNames.QUERY]: { type: "string" };
     [BaseFiltersNames.ORDER]: { parser: (value: string) => Order };
 };
 
-export const BASE_FILTERS_DECLARATION: Filters = {
+export const BASE_FILTERS_DECLARATION: BaseFilters = {
     [BaseFiltersNames.QUERY]: { type: "string" },
     [BaseFiltersNames.ORDER]: {
         parser: (value) => {
