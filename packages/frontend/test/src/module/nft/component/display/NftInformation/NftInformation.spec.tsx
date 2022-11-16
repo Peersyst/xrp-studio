3b737a344a837f1ea3c060ba8d400dba495e3329
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-11-16 12:41:50 +0100

    NftInformation with updated tests

--- packages/frontend/test/src/module/nft/component/display/NftInformation/NftInformation.spec.tsx
+++ packages/frontend/test/src/module/nft/component/display/NftInformation/NftInformation.spec.tsx
@@ -5,6 +5,26 @@ import { CreateNftDraftRequestMock, CreateNftMetadataRequestMock } from "test-mo
 import * as Recoil from "recoil";
 
 describe("NftInformation tests", () => {
+    const COLLECTION_NFT = "collection-name";
+
+    test("Renders correctly with data", () => {
+        const createNftDraftRequestMock = new CreateNftDraftRequestMock({
+            issuer: "0x",
+            transferFee: 1,
+            flags: undefined,
+            taxon: 1,
+            metadata: new CreateNftMetadataRequestMock({ name: "nft-name", image: "src-image" }),
+        });
+        const usePublishNftStateMock = jest
+            .spyOn(Recoil, "useRecoilValue")
+            .mockReturnValue({ data: createNftDraftRequestMock, collection: COLLECTION_NFT });
+        render(<NftInformation />);
+        expect(usePublishNftStateMock).toHaveBeenCalled();
+        expect(screen.getByText("nft-name")).toBeInTheDocument();
+        expect(screen.getByText(COLLECTION_NFT)).toBeInTheDocument();
+        expect(screen.getByText("0x")).toBeInTheDocument();
+        expect(screen.getByText("1%")).toBeInTheDocument();
+    });
     test("Renders correctly without data", () => {
         const createNftDraftRequestMock = new CreateNftDraftRequestMock({
             issuer: undefined,
