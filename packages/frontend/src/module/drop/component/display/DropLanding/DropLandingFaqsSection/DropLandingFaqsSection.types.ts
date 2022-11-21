020de04c4a2771b6d5c55dc133d4c60d7e5497d3
pablo <silvap.javier@gmail.com>
2022-11-21 15:33:21 +0100

    fix post merge

--- packages/frontend/src/module/drop/component/display/DropLanding/DropLandingFaqsSection/DropLandingFaqsSection.types.ts
+++ packages/frontend/src/module/drop/component/display/DropLanding/DropLandingFaqsSection/DropLandingFaqsSection.types.ts
@@ -1,9 +1,9 @@
+import { Loosen } from "@peersyst/react-types";
 import { FaqsDto } from "module/api/service";
 import { CSSProperties } from "react";
 
 export interface DropLandingFaqsSectionProps {
-    faqs?: FaqsDto[];
+    faqs: Loosen<FaqsDto, "id">[];
     className?: string;
     style?: CSSProperties;
-    loading?: boolean;
 }
