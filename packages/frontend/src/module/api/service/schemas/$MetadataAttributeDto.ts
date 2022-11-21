fdc83dde69cfa602ed4f41cf4942e890a18a2452
AdriaCarrera <adria.cm95@gmail.com>
2022-11-21 18:36:06 +0100

    Fix frontend models

---
+++ packages/frontend/src/module/api/service/schemas/$MetadataAttributeDto.ts
@@ -0,0 +1,19 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export const $MetadataAttributeDto = {
+    properties: {
+        traitType: {
+            type: 'string',
+            isRequired: true,
+        },
+        value: {
+            type: 'string',
+            isRequired: true,
+        },
+        displayType: {
+            type: 'string',
+            isNullable: true,
+        },
+    },
+} as const;
