31f1a1eb8f20e836d73acdab8d248e6ea252fdfa
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-12-07 08:42:42 +0100

    ArtistCard skeleton and wrapped link

--- packages/frontend/src/module/landing/display/ArtistCard/ArtistCard.tsx
+++ packages/frontend/src/module/landing/display/ArtistCard/ArtistCard.tsx
@@ -11,23 +11,23 @@ const ArtistCard = forwardRef(({ artist, loading = false }: WithLoading<LandingP
     const translate = useTranslate();
 
     return (
-        <Row gap="1rem" alignItems="center" ref={ref} css={{ minWidth: "16.5rem", maxWidth: "16.5rem" }}>
-            <Avatar img={image} alt="artist-image" loading={loading} size={"md"} />
-            <Col gap="1rem" alignItems="center">
-                <Skeleton loading={loading}>
-                    <Link to={UserRoutes.PROFILE.replace(":address", address)} variant="body2">
+        <Link to={UserRoutes.PROFILE.replace(":address", address)} variant="body2">
+            <Row gap="1rem" alignItems="center" ref={ref} css={{ minWidth: "16.5rem", maxWidth: "16.5rem" }}>
+                <Avatar img={image} alt="artist-image" loading={loading} size={"md"} />
+                <Col gap="1rem" alignItems="center">
+                    <Skeleton loading={loading} style={{ maxWidth: "8rem", minWidth: "8rem" }}>
                         <Col gap={"0.5rem"} css={{ maxWidth: "9.5rem" }}>
                             <Typography variant="body1" fontWeight={800} singleLine>
                                 {name}
                             </Typography>
                             <Typography variant="body2" light>
-                                {translate("nftsCount", { count: nftsCount ? nftsCount : 0 })}
+                                {translate("nftsCount", { count: nftsCount || 0 })}
                             </Typography>
                         </Col>
-                    </Link>
-                </Skeleton>
-            </Col>
-        </Row>
+                    </Skeleton>
+                </Col>
+            </Row>
+        </Link>
     );
 });
 
