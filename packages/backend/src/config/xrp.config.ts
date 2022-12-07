2789b0c7044f295a8a8f8ae9c8e699057adf3739
AdriaCarrera <adria.cm95@gmail.com>
2022-12-07 11:44:15 +0100

    Fix staging ami changing over time

--- packages/backend/src/config/xrp.config.ts
+++ packages/backend/src/config/xrp.config.ts
@@ -17,7 +17,7 @@ export default (secrets: AwsSecrets = {}): XrpConfig => {
                 production: "wss://xrplcluster.com",
             },
             startingLedgerIndex: {
-                default: 33263316,
+                default: 33484101,
                 production: 75184704,
             },
             minterSecret: {
