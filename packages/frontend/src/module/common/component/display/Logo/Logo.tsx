aef95bdb4548f67d689468edc641ea5631a4209f
pablo <silvap.javier@gmail.com>
2022-12-09 11:02:00 +0100

    Fix Styles in Header

--- packages/frontend/src/module/common/component/display/Logo/Logo.tsx
+++ packages/frontend/src/module/common/component/display/Logo/Logo.tsx
@@ -3,14 +3,17 @@ import { cx } from "@peersyst/react-utils";
 import { logo, logo_dark } from "images";
 import { LogoProps } from "module/common/component/display/Logo/Logo.types";
 import Link from "module/common/component/navigation/Link/Link";
+import useIsMobile from "module/common/hook/useIsMobile";
 
 export default function Logo({ className, style }: LogoProps): JSX.Element {
     const {
         palette: { mode },
     } = useTheme();
 
+    const isMobile = useIsMobile();
+
     return (
-        <Link to="/" type="router">
+        <Link to="/" type="router" style={{ width: isMobile ? "2.25rem" : "fit-content", marginRight: isMobile ? "1rem" : "0" }}>
             <img src={mode === "dark" ? logo : logo_dark} className={cx("Logo", className)} style={style} />
         </Link>
     );
--- packages/frontend/src/module/wallet/component/core/WalletNotConnected/WalletNotConnected.tsx
+++ packages/frontend/src/module/wallet/component/core/WalletNotConnected/WalletNotConnected.tsx
@@ -2,11 +2,18 @@ import { Typography } from "@peersyst/react-components";
 import ConnectXummButton from "module/wallet/component/input/ConnectXummButton/ConnectXummButton";
 import useTranslate from "module/common/hook/useTranslate";
 import { WalletNotConnectedRoot } from "./WalletNotConnected.styles";
+import { ExploreRoutes } from "module/explore/ExploreRouter";
+import { Link } from "react-router-dom";
 
 const WalletNotConnected = (): JSX.Element => {
     const translate = useTranslate();
     return (
         <WalletNotConnectedRoot gap="1rem" alignItems="center">
+            <Link to={ExploreRoutes.MAIN}>
+                <Typography variant="body1" fontWeight={500}>
+                    {translate("explore")}
+                </Typography>
+            </Link>
             <Typography variant="body1" fontWeight={500} light className="get-started-text">
                 {translate("letsGetStarted")}
             </Typography>
