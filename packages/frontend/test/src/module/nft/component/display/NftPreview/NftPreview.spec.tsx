5078119d71c5fa5bb72307738aeb0d489f5f4e27
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-12-12 17:44:57 +0100

    NftPreview test fix

--- packages/frontend/test/src/module/nft/component/display/NftPreview/NftPreview.spec.tsx
+++ packages/frontend/test/src/module/nft/component/display/NftPreview/NftPreview.spec.tsx
@@ -7,9 +7,10 @@ describe("NftPreview", () => {
     const nftMock = new NftDtoMock();
     const nftName = nftMock.metadata!.name!;
     const nftPreviewLink = `/nft/creation?id=${nftMock.id}`;
+    const mockNftDefaultPreviewIUrl = "url";
 
     test("Renders correctly without link", () => {
-        render(<NftPreview nft={nftMock} />);
+        render(<NftPreview nft={nftMock} defaultPreviewUrl={mockNftDefaultPreviewIUrl} />);
 
         expect(screen.queryByRole("link")).toBeNull();
         expect(screen.getByRole("img")).toHaveAttribute("alt", `${nftName}-preview-img`);
@@ -17,13 +18,13 @@ describe("NftPreview", () => {
     });
 
     test("Renders correctly with link", () => {
-        render(<NftPreview nft={nftMock} to={nftPreviewLink} />);
+        render(<NftPreview nft={nftMock} to={nftPreviewLink} defaultPreviewUrl={mockNftDefaultPreviewIUrl} />);
 
         expect(screen.queryByRole("link")).toHaveAttribute("href", nftPreviewLink);
     });
 
     test("Renders correctly with link", () => {
-        render(<NftPreview nft={nftMock} loading />);
+        render(<NftPreview nft={nftMock} loading defaultPreviewUrl={mockNftDefaultPreviewIUrl} />);
 
         expect(screen.getByText("loading name")).toBeInTheDocument();
     });
