14cfafe2193f68b1bdfa2d9ded296f7ad6c66973
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-12-13 11:16:30 +0100

    Error message showing when declining

--- packages/frontend/src/module/nft/hook/useNftStatePolling.ts
+++ packages/frontend/src/module/nft/hook/useNftStatePolling.ts
@@ -1,17 +1,15 @@
 import { NftDraftStatusDto, NftService } from "module/api/service";
 import { polling } from "@peersyst/react-utils";
-import useTranslate from "module/common/hook/useTranslate";
+import { AppError } from "../../../query/AppError";
 
 export interface UseNftStatePolling {
     fetch: () => Promise<NftDraftStatusDto[]> | undefined;
 }
 
 export default function (id: undefined | number): UseNftStatePolling {
-    const translate = useTranslate("error");
-
     const handleStatus = (res: NftDraftStatusDto[]) => {
         const status = res[0].status;
-        if (status === "failed") throw new Error(translate("nftMinting"));
+        if (status === "failed") throw new AppError("nftMinting");
         return status === "pending";
     };
 
