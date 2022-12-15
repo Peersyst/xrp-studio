979fe5f1da281146e92f4a9ed1595b0c9fe22f9b
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-12-15 18:30:00 +0100

    BasePage created

---
+++ packages/frontend/src/module/drop/page/MyDropsPage/MyDropsPageContent/MyDropsPageContent.tsx
@@ -0,0 +1,7 @@
+import PageContent from "module/common/component/layout/PageContent/PageContent";
+
+const MyDropsPageContent = (): JSX.Element => {
+    return <PageContent>Drops</PageContent>;
+};
+
+export default MyDropsPageContent;
