e3a0d1129b28e0a7137efe13ae45a92f6ebbdb20
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-12-23 11:54:15 +0100

    MyCollectionsPageHeader test

--- packages/frontend/test/src/module/collection/component/layout/MyCollectionsPageHeader/MyCollectionsPageHeader.spec.tsx
+++ packages/frontend/test/src/module/collection/component/layout/MyCollectionsPageHeader/MyCollectionsPageHeader.spec.tsx
@@ -3,7 +3,6 @@ import MyCollectionsPageHeader from "module/collection/component/layout/MyCollec
 import * as ReactRouterDom from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 import { CollectionRoutes } from "module/collection/CollectionRouter";
-import * as recoil from "recoil";
 
 describe("Test for MyCollectionsPageHeader", () => {
     test("Renders correctly", () => {
@@ -13,16 +12,14 @@ describe("Test for MyCollectionsPageHeader", () => {
     });
     test("Navigates to the create collection page", () => {
         const mockedNavigate = jest.fn();
-        const mockedResetRecoilState = jest.fn();
+
         jest.spyOn(ReactRouterDom, "useNavigate").mockReturnValue(mockedNavigate);
-        jest.spyOn(recoil, "useResetRecoilState").mockReturnValue(mockedResetRecoilState);
 
         const screen = render(<MyCollectionsPageHeader />);
 
         const button = screen.getByRole("button", { name: translate("createCollection") });
         userEvent.click(button);
 
-        expect(mockedResetRecoilState).toHaveBeenCalled();
         expect(mockedNavigate).toHaveBeenCalledWith(CollectionRoutes.CREATE_COLLECTION);
     });
 });
