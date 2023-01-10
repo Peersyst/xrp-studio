af46be11274d0b7f7be0711971491a5a29ce7a20
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-01-10 13:24:59 +0100

    Fix decline sign in error

--- packages/frontend/src/module/wallet/hook/useConnectToXumm/useConnectToXumm.ts
+++ packages/frontend/src/module/wallet/hook/useConnectToXumm/useConnectToXumm.ts
@@ -1,15 +1,18 @@
-import { useModal } from "@peersyst/react-components";
+import { useModal, useToast } from "@peersyst/react-components";
 import ConnectXummModal from "module/wallet/component/feedback/ConnectXummModal/ConnectXummModal";
 import { walletState } from "module/wallet/state/WalletState";
 import { useState } from "react";
 import { useSetRecoilState } from "recoil";
-import { SignInResponse, useSignIn, VerifySignInResponse } from "xumm-react";
+import { SignInResponse, TransactionRequestStatus, useSignIn, VerifySignInResponse } from "xumm-react";
 import { UseConnectToXummProps, UseConnectToXummReturnValue } from "./useConnectToXumm.types";
+import useTranslate from "module/common/hook/useTranslate";
 
 const useConnectToXumm = ({ callback }: UseConnectToXummProps): UseConnectToXummReturnValue => {
     const [xummQrUrl, setXummQrUrl] = useState<UseConnectToXummReturnValue["xummQrUrl"]>();
     const setWalletState = useSetRecoilState(walletState);
     const { hideModal } = useModal();
+    const { showToast } = useToast();
+    const translateError = useTranslate("error");
 
     const onSignIn = ({ xummPayload }: SignInResponse) => {
         setXummQrUrl(xummPayload.refs.qr_png);
@@ -25,7 +28,14 @@ const useConnectToXumm = ({ callback }: UseConnectToXummProps): UseConnectToXumm
         callback && callback();
     };
 
-    const { signIn, verifySignInLoading } = useSignIn({ onSignIn, onSignInVerified });
+    const onSignatureResolved = (status: TransactionRequestStatus) => {
+        if (status === "declined") showToast(translateError("xummSignInRequestDeclined"), { type: "error" });
+        else if (status === "cancelled") showToast(translateError("xummSignInRequestCancelled"), { type: "error" });
+        else if (status === "expired") showToast(translateError("xummSignInRequestExpired"), { type: "error" });
+        hideModal(ConnectXummModal.id);
+    };
+
+    const { signIn, verifySignInLoading } = useSignIn({ onSignIn, onSignInVerified, onSignatureResolved });
 
     return { signIn, xummQrUrl, showLoading: verifySignInLoading };
 };
