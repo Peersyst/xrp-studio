db21317ef0f04f8c5319c7014670bad87f76986b
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-01-10 13:19:14 +0100

    Fix app bar links

--- packages/frontend/src/module/common/component/navigation/MenuLinks/MenuLinks.tsx
+++ packages/frontend/src/module/common/component/navigation/MenuLinks/MenuLinks.tsx
@@ -2,27 +2,30 @@ import { Typography } from "@peersyst/react-components";
 import useTranslate from "module/common/hook/useTranslate";
 import useWallet from "module/wallet/hook/useWallet";
 import { APPBAR_TABS } from "../AppBar/AppBarTabs";
-import ConditionalLink from "../ConditionalLink/ConditionalLink";
+import Link from "../Link/Link";
+import { MenuLinksRoot } from "./MenuLinks.styles";
 import { MenuLinksProps } from "./MenuLinks.types";
+import { useLocation } from "react-router-dom";
 
 const MenuLinks = ({ onClick }: MenuLinksProps): JSX.Element => {
     const { isLogged } = useWallet();
     const translate = useTranslate();
+    const location = useLocation();
 
     return (
-        <>
+        <MenuLinksRoot>
             {isLogged ? (
                 APPBAR_TABS.map((item) => (
-                    <ConditionalLink key={item.label} condition={isLogged} to={item.path} onClick={onClick ? () => onClick() : undefined}>
+                    <Link key={item.label} to={item.path} onClick={onClick ? () => onClick() : undefined}>
                         <Typography variant="body1" color={location.pathname.includes(item.path) ? "black.0" : "black.40"} fontWeight={500}>
                             {translate(item.label)}
                         </Typography>
-                    </ConditionalLink>
+                    </Link>
                 ))
             ) : (
                 <></>
             )}
-        </>
+        </MenuLinksRoot>
     );
 };
 export default MenuLinks;
