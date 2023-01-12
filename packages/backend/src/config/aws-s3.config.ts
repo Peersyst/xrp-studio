51c6c1c9a8dc5d90b1ce7148c99beff2ca8c9487
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-01-12 17:31:50 +0100

    Fix aws config

--- packages/backend/src/config/aws-s3.config.ts
+++ packages/backend/src/config/aws-s3.config.ts
@@ -11,13 +11,13 @@ interface AwsS3Config {
 export default (secrets: AwsSecrets): AwsS3Config => {
     return buildConfig<AwsS3Config>({
         region: {
-            default: "eu-west-1" || secrets.AWS_S3_REGION,
+            default: process.env.AWS_S3_REGION || secrets.AWS_S3_REGION,
         },
         accessKeyId: {
-            default: "awsAccessKeyId" || secrets.AWS_S3_ACCESS_KEY_ID,
+            default: process.env.AWS_S3_ACCESS_KEY_ID || secrets.AWS_S3_ACCESS_KEY_ID,
         },
         secretAccessKey: {
-            default: "awsSecretAccessKey" || secrets.AWS_S3_SECRET_ACCESS_KEY,
+            default: process.env.AWS_S3_SECRET_ACCESS_KEY || secrets.AWS_S3_SECRET_ACCESS_KEY,
         },
         bucketName: {
             default: process.env.AWS_S3_BUCKET_NAME || secrets.AWS_S3_BUCKET_NAME,
