17f1679dad19d9a57cd2d396e1501f231bcebca5
AdriaCarrera <adria.cm95@gmail.com>
2023-02-13 11:04:13 +0100

    Fix migrations

--- packages/backend/src/database/migrations/1676027810956-nft_metadata_attribute_text.ts
+++ packages/backend/src/database/migrations/1676027810956-nft_metadata_attribute_text.ts
@@ -4,20 +4,20 @@ export class nftMetadataAttributeText1676027810956 implements MigrationInterface
     name = "nftMetadataAttributeText1676027810956";
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" DROP COLUMN "trait_type"`);
-        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" ADD "trait_type" text`);
-        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" DROP COLUMN "value"`);
-        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" ADD "value" text`);
-        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" DROP COLUMN "display_type"`);
-        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" ADD "display_type" text`);
+        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" ALTER COLUMN "trait_type" TYPE TEXT USING "trait_type" :: TEXT`);
+        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" ALTER COLUMN "value" TYPE TEXT USING "value" :: TEXT`);
+        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" ALTER COLUMN "display_type" TYPE TEXT USING "display_type" :: TEXT`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" DROP COLUMN "display_type"`);
-        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" ADD "display_type" character varying(255)`);
-        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" DROP COLUMN "value"`);
-        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" ADD "value" character varying(255)†`);
-        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" DROP COLUMN "trait_type"`);
-        await queryRunner.query(`ALTER TABLE "nft_metadata_attribute" ADD "trait_type" character varying(255)`);
+        await queryRunner.query(
+            `ALTER TABLE "nft_metadata_attribute" ALTER COLUMN "trait_type" TYPE varying(255) USING "trait_type" :: varying(255)`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "nft_metadata_attribute" ALTER COLUMN "value" TYPE varying(255) USING "value" :: varying(255)`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "nft_metadata_attribute" ALTER COLUMN "display_type" TYPE varying(255) USING "display_type" :: varying(255)`,
+        );
     }
 }
