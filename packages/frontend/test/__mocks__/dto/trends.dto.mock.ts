5e7a871a314f987564d7d46a628ef3b5c310c563
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-02-13 15:41:31 +0100

    Fix tests

--- packages/frontend/test/__mocks__/dto/trends.dto.mock.ts
+++ packages/frontend/test/__mocks__/dto/trends.dto.mock.ts
@@ -1,4 +1,4 @@
-import { CollectionDto, NftDto, TrendsDto, UserDto } from "module/api/service";
+import { CollectionDto, DropDto, NftDto, TrendsDto, UserDto } from "module/api/service";
 import BaseMock from "../base.mock";
 import { TrendService } from "module/api/service";
 
@@ -6,12 +6,14 @@ export class TrendsDtoMock extends BaseMock implements TrendsDto {
     nfts: Array<NftDto>;
     collections: Array<CollectionDto>;
     artists: Array<UserDto>;
+    drops: Array<DropDto>;
 
-    constructor({ nfts = [], collections = [], artists = [] }: Partial<TrendsDto> = {}) {
+    constructor({ nfts = [], collections = [], artists = [], drops = [] }: Partial<TrendsDto> = {}) {
         super();
         this.nfts = nfts;
         this.collections = collections;
         this.artists = artists;
+        this.drops = drops;
         this.mock = jest.spyOn(TrendService, "trendControllerGetTrends").mockResolvedValue(this);
     }
 }
