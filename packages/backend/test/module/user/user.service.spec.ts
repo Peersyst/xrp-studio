9c745f482059a083c65baecc764f554fd74c8998
AgustinMJ <agustinmillanjimenez+github@gmail.com>
2023-11-20 16:18:20 +0100

    feat: Trend only returns verified artists data

--- packages/backend/test/module/user/user.service.spec.ts
+++ packages/backend/test/module/user/user.service.spec.ts
@@ -49,7 +49,6 @@ describe("UserService", () => {
                     address: ADDRESS,
                     image: "default_profile_img_url",
                     header: "default_header_img_url",
-                    verifiedArtist: true,
                 }),
             );
             expect(userRepositoryMock.save).toHaveBeenCalledWith({
@@ -57,7 +56,6 @@ describe("UserService", () => {
                 address: ADDRESS,
                 image: "default_profile_img_url",
                 header: "default_header_img_url",
-                verifiedArtist: true,
             });
         });
     });
