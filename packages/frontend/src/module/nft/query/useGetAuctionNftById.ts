b921c30609d39621f1209105cdb058913bd5c6fa
pablo <silvap.javier@gmail.com>
2023-12-01 08:58:23 +0100

    Add config interval in auction

--- packages/frontend/src/module/nft/query/useGetAuctionNftById.ts
+++ packages/frontend/src/module/nft/query/useGetAuctionNftById.ts
@@ -2,10 +2,11 @@ import { QueryResult } from "query-utils";
 import { AuctionDto, NftService } from "module/api/service";
 import { useQuery } from "react-query";
 import Queries from "../../../query/queries";
+import { config } from "config";
 
 export default function (id: string | undefined, date: number): QueryResult<AuctionDto> {
     return useQuery([Queries.NFT_AUCTION, id], () => NftService.nftControllerGetAuctionNft(id!, date), {
         enabled: id !== undefined,
-        refetchInterval: 3000,
+        refetchInterval: config.auctionRefechInterval,
     });
 }
